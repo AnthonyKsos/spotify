4efcb7b0edaf11b1d7a76ad9d8b64e06c5a2676a
antho <128251663+AnthonyKsos@users.noreply.github.com>
2024-04-19 09:25:00 -0500

    update files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Array que contendrá valores de rutas, allí se declaran las rutas
+// La ruta es un objeto que esta constituido de un path, relacionado a un component
+const routes: Routes = [
+  {
+    path: "", // localhost:4200/
+    loadChildren:() => import('./modules/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path:'', // Todo: localhost:4200/
+    loadChildren:() => import('./modules/home/home.module').then(m => m.HomeModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
